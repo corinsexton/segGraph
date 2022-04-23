23a724de4417a5cd963e58860613c1ab866d1cb0
corinsexton <corinsexton@gmail.com>
2022-04-23 09:14:12 -0700

    add gem

---
+++ 3_embedding/gem/README.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+module load conda
+conda activate gem
+
+
+# PROBLEMS: had to hand compile gf for graph factorization
+#		https://github.com/palash1992/GEM/issues/86
+
+# had problems with keras and tensorflow import of SGD, couldn't run SDNE
+
---
+++ 3_embedding/gem/gem/version.py
@@ -0,0 +1,11 @@
+
+    # THIS FILE IS GENERATED FROM GEM SETUP.PY
+    short_version = '1.0.0'
+    version = '1.0.0'
+    full_version = '1.0.0'
+    git_revision = 'ea1cb5f9f28c0d2f49646442ccfd2b6ea7b6cab8'
+    release = 'True'
+    if not release:
+        version = full_version
+        short_version += ".dev"
+    
